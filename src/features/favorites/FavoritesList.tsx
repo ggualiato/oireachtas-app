eac6c7b0545f093ffae8e25900eafdf2a640d2cf
Giovanni Gualiato <ggualiato@gmail.com>
2024-05-22 00:24:46 +0100

    feat: favourite list

--- src/features/favorites/FavoritesList.tsx
+++ src/features/favorites/FavoritesList.tsx
@@ -1,7 +1,28 @@
+import { IconButton, List, ListItem, ListItemText } from "@mui/material";
 import { useFavoritesContext } from "./useFavoritesContext";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const FavoritesList = () => {
-  const { favorites } = useFavoritesContext();
-  console.log("x", favorites);
-  return <>Hello</>;
+  const { favorites, unfavoriteBill } = useFavoritesContext();
+  return (
+    <div style={{ width: 500, height: 200 }}>
+      <List>
+        {favorites.map((fav) => (
+          <ListItem
+            secondaryAction={
+              <IconButton
+                edge="start"
+                aria-label="delete"
+                onClick={() => unfavoriteBill(fav.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={fav.id} secondary={"Secondary text"} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 };
