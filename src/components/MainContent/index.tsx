252318f66c5e5b240f5ecb40c98ea7619e5dfa03
Giovanni Gualiato <ggualiato@gmail.com>
2024-05-22 22:49:09 +0100

    fix: wrap components with LegislationProvider

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Container, Tab, Tabs } from "@mui/material";
 import { CustomTabPanel } from "../CustomTabPanel";
-import { BillList } from "../../features/bills/BillList";
+import { BillList } from "../../features/bills/BillsView";
 import { FavoritesProvider } from "../../features/favorites/FavoritesContext";
 import { FavoritesList } from "../../features/favorites/FavoritesList";
+import { LegislationProvider } from "../../features/bills/LegislationContext";
 
 export const MainContent = () => {
   const [value, setValue] = useState(0);
@@ -14,20 +15,22 @@ export const MainContent = () => {
 
   return (
     <div>
-      <FavoritesProvider>
-        <Container maxWidth="sm">
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Bills" id="bills-tab" />
-            <Tab label="Favourites" id="favourites-tab" />
-          </Tabs>
-          <CustomTabPanel value={value} index={0}>
-            <BillList />
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={1}>
-            <FavoritesList />
-          </CustomTabPanel>
-        </Container>
-      </FavoritesProvider>
+      <LegislationProvider>
+        <FavoritesProvider>
+          <Container maxWidth="sm">
+            <Tabs value={value} onChange={handleChange}>
+              <Tab label="Bills" id="bills-tab" />
+              <Tab label="Favourites" id="favourites-tab" />
+            </Tabs>
+            <CustomTabPanel value={value} index={0}>
+              <BillList />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              <FavoritesList />
+            </CustomTabPanel>
+          </Container>
+        </FavoritesProvider>
+      </LegislationProvider>
     </div>
   );
 };
