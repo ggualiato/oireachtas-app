4df32fb983041056a7b52e174261c440ea3c01a5
Giovanni Gualiato <ggualiato@gmail.com>
2024-05-23 18:18:23 +0100

    fix: make bill tab selected as default

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -4,31 +4,29 @@ import { useRouteMatch } from "../../hooks/useRouteMatch";
 
 export const MainContent = () => {
   const routeMatch = useRouteMatch(["/bills", "/favourites"]);
-  const currentTab = routeMatch?.pattern?.path;
+  const currentTab = routeMatch?.pattern?.path ?? "/bills";
 
   return (
-    <div>
-      <Container maxWidth="sm">
-        <Tabs value={currentTab}>
-          <Tab
-            label="Bills"
-            value={"/bills"}
-            id="bills-tab"
-            to={`bills`}
-            component={Link}
-          />
-          <Tab
-            label="Favourites"
-            value={"/favourites"}
-            id="favourites-tab"
-            to="favourites"
-            component={Link}
-          />
-        </Tabs>
-        <Container>
-          <Outlet />
-        </Container>
+    <Container maxWidth="md">
+      <Tabs value={currentTab}>
+        <Tab
+          label="Bills"
+          value={"/bills"}
+          id="bills-tab"
+          to={`bills`}
+          component={Link}
+        />
+        <Tab
+          label="Favourites"
+          value={"/favourites"}
+          id="favourites-tab"
+          to="favourites"
+          component={Link}
+        />
+      </Tabs>
+      <Container style={{ marginTop: "5px" }}>
+        <Outlet />
       </Container>
-    </div>
+    </Container>
   );
 };
