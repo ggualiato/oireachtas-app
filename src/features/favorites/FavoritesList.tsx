380c7909e3aec07fb47f7708e78c9494a30b94be
Giovanni Gualiato <ggualiato@gmail.com>
2024-05-23 17:56:29 +0100

    feat: no favourites message

--- src/features/favorites/FavoritesList.tsx
+++ src/features/favorites/FavoritesList.tsx
@@ -1,12 +1,22 @@
-import { IconButton, List, ListItem, ListItemText } from "@mui/material";
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useFavoritesContext } from "./useFavoritesContext";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 export const FavoritesList = () => {
   const { favorites, unfavoriteBill } = useFavoritesContext();
+  const noFavoritesYet = favorites.length === 0;
 
   return (
     <div style={{ width: 500, height: 200 }}>
+      {noFavoritesYet && (
+        <Typography sx={{ mt: 2 }}>{"There is no favorites yet"}</Typography>
+      )}
       <List>
         {favorites.map((favorite) => (
           <ListItem
