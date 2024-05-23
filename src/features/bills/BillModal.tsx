a2807be4f997c89fd43f6fbcf2cbfa08f2134569
Giovanni Gualiato <ggualiato@gmail.com>
2024-05-23 18:46:35 +0100

    feat: show long title

--- src/features/bills/BillModal.tsx
+++ src/features/bills/BillModal.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Modal, Tab, Tabs, Typography } from "@mui/material";
 import { useState } from "react";
 import { CustomTabPanel } from "../../components/CustomTabPanel";
 import { Bill } from "../../domain/legislation";
@@ -10,7 +10,7 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "2px solid #0288d1",
   boxShadow: 24,
   p: 4,
   borderRadius: 2,
@@ -30,17 +30,24 @@ export const BillModal = ({ bill }: BillModalProps) => {
         <Tab label="Gaeilge" id="gaeilge-tab" />
       </Tabs>
       <CustomTabPanel value={value} index={0}>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {bill.shortTitleEn}
+        <Typography variant="h6" sx={{ mt: 2 }}>
+          {bill?.shortTitleEn}
         </Typography>
-        {/* <Typography id="modal-modal-title" variant="h6" component="h2">
-          <div dangerouslySetInnerHTML={{ __html: bill.longTitleEn }}></div>
-        </Typography> */}
+        <div style={{ height: "300px", overflow: "auto" }}>
+          <Typography>
+            <span dangerouslySetInnerHTML={{ __html: bill.longTitleEn }}></span>
+          </Typography>
+        </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        <Typography variant="h6" sx={{ mt: 2 }}>
           {bill.shortTitleGa}
         </Typography>
+        <div style={{ height: "300px", overflow: "auto" }}>
+          <Typography>
+            <span dangerouslySetInnerHTML={{ __html: bill.longTitleGa }}></span>
+          </Typography>
+        </div>
       </CustomTabPanel>
     </Box>
   );
